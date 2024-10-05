be96db49e3418c294f4ed6dcf9c277135d522a21
Sean W <we.sean94@gmail.com>
2024-10-04 18:52:51 -0700

    lil jump

---
+++ Assets/_Code/PlayerCharacter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public ContactFilter2D groundedContactFilter;
+    public float jumpSpeed;
+
+    private List<RaycastHit2D> _raycastHits = new();
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            Vector2 vel = myRigidbody.velocity;
+            vel.y = jumpSpeed;
+            myRigidbody.velocity = vel;
+        }
+    }
+
+    public bool IsGrounded()
+    {
+        int count = Physics2D.BoxCast(myRigidbody.position - Vector2.up * 0.5f, new Vector2(1f, 0.1f), 0f, Vector2.down, groundedContactFilter, _raycastHits, 0.01f);
+        return count > 0;
+    }
+}
